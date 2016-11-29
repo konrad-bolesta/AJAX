17e9894e7d09269d932043553dcaf8cfe8151f88
Konrad <konrad.bolesta@gmail.com>
2016-11-29 23:24:28 +0100

    v2

--- scripts.js
+++ scripts.js
@@ -27,10 +27,43 @@
     }
 
     function showCountriesList(resp) {
+        if(resp.length === 1) {
             countriesList.empty();
-        resp.forEach(function(item){
-            $("<li>").text(item.name + " ("+item.capital+")").appendTo(countriesList);
-        });
+            resp.forEach(function(item){
+                $("<li>").text("Name: " + item.name).appendTo(countriesList);
+                $("<li>").text("Capital: " + item.capital).appendTo(countriesList);
+                $("<li>").text("Area: " + item.area).appendTo(countriesList);
+                $("<li>").text("Population: " + item.population).appendTo(countriesList);
+                $("<li>").text("Native name: " + item.nativeName).appendTo(countriesList);
+                $("<li>").text("Demonym: " + item.demonym).appendTo(countriesList);
+                $("<li>").text("Currencies: " + item.currencies).appendTo(countriesList);
+                $("<li>").text("Region: " + item.region).appendTo(countriesList);
+                $("<li>").text("Subregion: " + item.subregion).appendTo(countriesList);
+                $("<li>").text("Alpha codes: " + item.alpha2Code + ", " + item.alpha3Code).appendTo(countriesList);
+                $("<li>").text("Numeric code: " + item.numericCode).appendTo(countriesList);
+                $("<li>").text("Caling codes: " + item.callingCodes).appendTo(countriesList);
+                $("<li>").text("Top-Level Domain: " + item.topLevelDomain).appendTo(countriesList);
+            });
+        } else {
+            countriesList.empty();
+            resp.forEach(function(item){
+                $("<li>").text(item.name).appendTo(countriesList).css({"cursor": "pointer"}).click(function (){
+                    countriesList.empty();
+                    $("<li>").text("Name: " + item.name).appendTo(countriesList);
+                    $("<li>").text("Capital: " + item.capital).appendTo(countriesList);
+                    $("<li>").text("Area: " + item.area).appendTo(countriesList);
+                    $("<li>").text("Population: " + item.population).appendTo(countriesList);
+                    $("<li>").text("Native name: " + item.nativeName).appendTo(countriesList);
+                    $("<li>").text("Demonym: " + item.demonym).appendTo(countriesList);
+                    $("<li>").text("Currencies: " + item.currencies).appendTo(countriesList);
+                    $("<li>").text("Region: " + item.region).appendTo(countriesList);
+                    $("<li>").text("Subregion: " + item.subregion).appendTo(countriesList);
+                    $("<li>").text("Alpha codes: " + item.alpha2Code + ", " + item.alpha3Code).appendTo(countriesList);
+                    $("<li>").text("Numeric code: " + item.numericCode).appendTo(countriesList);
+                    $("<li>").text("Caling codes: " + item.callingCodes).appendTo(countriesList);
+                    $("<li>").text("Top-Level Domain: " + item.topLevelDomain).appendTo(countriesList);
+                });
+            });
+        }
     }
-
 });
